b2993c40e6a1650bc7d47e70815824480d84e525
Qsp <Qsp@DESKTOP-6P3VQFR>
2025-04-05 18:45:21 +0530

    second commit added project

---
+++ Log2j4/src/main/java/Demo/face.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class face {
+
+//	private static Object By;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver=new ChromeDriver();
+		 driver.manage().window().maximize();
+		 driver.get("https://www.facebook.com/r.php?entry_point=login");
+		 driver.findElement(By.name("firstname")).click();
+		 driver.findElement(By.name("lastname")).click();
+		 driver.quit();
+	}
+
+}
